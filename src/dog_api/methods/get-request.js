76de51805d304c4b5cbb47ecd9ad7f45cdc67806
Jasper-Dewispelaere <jasper.dewispelaere@student.howest.be>
2023-05-10 14:40:20 +0200

    Update get-request

--- src/dog_api/methods/get-request.js
+++ src/dog_api/methods/get-request.js
@@ -1,1 +1,12 @@
-module.exports = (req, res) => {};
+module.exports = (req, res) => {
+    if (req.url == "/api/dogs") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.write(JSON.stringify(req.dogs));
+        res.end();
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ title: "Not Found 💀 ", message: "Route not found" }));
+    }
+};
